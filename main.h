d048f783081a7411d4c7cca43510424347f1ca43
smuhatia <smuhatia>
2023-05-16 19:44:26 +0000

    edit on main.h

--- main.h
+++ main.h
@@ -1,11 +1,13 @@
 #ifndef "MAIN_H"
 #define "MAIN_H"
+#define MAX_COMMAND 10
 
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
 #include <sys/wait.h>
+
 void display_prompt(char **av, char **env);
 
 #endif
