c3f5e1c3807840994509274404d4f380df588acf
smuhatia <smuhatia>
2023-05-24 11:04:18 +0000

    builtin commands

---
+++ builtin.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * execute_builtins - handles builtin functions
+ * @tokens: tokenized commands
+ * @env: contains values of environ
+ */
+void execute_builtins(char **tokens, char **env)
+{
+	if (my_strcmp(tokens[0], "exit") == 0) /* check if command is "exit" */
+	{
+		execute_exit(tokens);
+	}
+	else if (my_strcmp(tokens[0], "env") == 0) /* env built-in command */
+	{
+		execute_env(env);
+	}
+
+}
+
